7562edcb33398dbcfcc89d6d90aa5e1aefb497e8
Rohail Najam <rohail_najam@hotmail.com>
2017-06-22 13:09:28 +0500

    Added HTML Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * Created by Rohail on 6/19/2017.
  */
 
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   context: __dirname + '/src',
   entry : {
@@ -11,5 +11,12 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: __dirname + '/dist',
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title : 'My Title App',
+      template:'../index.ejs',
+      hash : true
+    })
+  ]
 };
